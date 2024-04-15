57aac7cf58457fd38129fe896df603870152fc0a
ZhijieXiong <18800118477@163.com>
2024-04-15 10:44:41 +0800

    update

--- gpt_evaluate.py
+++ gpt_evaluate.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     json_file_path = r"F:\code\myProjects\kaggle-AES2-competition\output\gpt-4-response\zero_shot.json"
     gpt_response = load_json(json_file_path)
     evaluate_result = evaluate(gpt_response)
-    print(f"QWK: {evaluate_result['QWK']}, ACC: {evaluate_result['ACC']}, F1 Score: {evaluate_result['F1']}")
+    print(f"num of sample: {len(gpt_response)}, QWK: {evaluate_result['QWK']}, ACC: {evaluate_result['ACC']}, F1 Score: {evaluate_result['F1']}")
