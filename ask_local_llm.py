d58380895dc8fef3f77ac03c69f3ae23bbd7b63f
ZhijieXiong <18800118477@163.com>
2024-04-24 21:26:54 +0800

    update llama zero shot evaluation

---
+++ ask_local_llm.py
@@ -0,0 +1,137 @@
+import json
+import re
+import time
+import argparse
+import os
+import transformers
+import torch
+import torch.distributed as dist
+
+from transformers import AutoTokenizer
+
+from model.llama.generation import Llama
+from util import load_csv, load_json, write_json, get_now_time
+
+
+def extract_score_explanation(gpt_response_):
+    try:
+        score_ = int(re.search("\$\$(\d)\$\$", gpt_response_).groups()[0])
+        explanation_ = gpt_response_.replace(f"$${score_}$$", "").strip(" ").strip(".").strip(",").strip(" ").strip(".").strip(",")
+        return False, gpt_response_, score_, explanation_
+    except:
+        return True, gpt_response_, None, None
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--prompts_path", type=str,
+                        default=r"F:\code\myProjects\kaggle-AES2-competition\prompts.json")
+    parser.add_argument("--model_dir", type=str,
+                        default=r"F:\code\myProjects\kaggle-AES2-competition\llama2-7b-hf")
+    parser.add_argument("--data_dir", type=str,
+                        default=r"F:\code\myProjects\kaggle-AES2-competition\data\processed")
+    parser.add_argument("--output_dir", type=str,
+                        default=r"F:\code\myProjects\kaggle-AES2-competition\output")
+    parser.add_argument("--data_name", type=str, default="kaggle-AES2024")
+    parser.add_argument("--num_ask", type=int, default=10)
+
+    # model params
+    parser.add_argument("--temperature", type=float, default=0.6)
+    parser.add_argument("--top_p", type=float, default=0.9)
+    parser.add_argument("--max_seq_len", type=int, default=4096)
+    parser.add_argument("--max_batch_size", type=int, default=8)
+
+    args = parser.parse_args()
+    params = vars(args)
+
+    # ---------------------------------------参数处理---------------------------------------
+    with open(params["prompts_path"], "r") as f:
+        PROMPTS = json.load(f)
+
+    DATA = load_csv(os.path.join(params["data_dir"], params["data_name"], "data.csv"))
+    DATA["score"] = DATA["score"] + 1
+
+    MODEL_DIR = params["model_dir"]
+    MODEL_NAME = os.path.basename(MODEL_DIR)
+    assert os.path.exists(MODEL_DIR), f"{MODEL_NAME} is not supported"
+    IS_HF = MODEL_NAME.split("-")[-1] == "hf"
+    OUTPUT_DIR = os.path.join(params["output_dir"], f"{MODEL_NAME}-response")
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+    NUM_ASK = params["num_ask"]
+    # -------------------------------------------------------------------------------------
+
+    # load pipline or generator
+    if IS_HF:
+        model = transformers.pipeline(
+            "text-generation",
+            model=MODEL_DIR,
+            torch_dtype=torch.float16,
+            # Do not use `device_map` AND `device` at the same time as they will conflict
+            device_map="auto",
+        )
+        tokenizer = AutoTokenizer.from_pretrained(MODEL_DIR)
+    else:
+        model = Llama.build(
+            ckpt_dir=MODEL_DIR,
+            tokenizer_path=os.path.join(MODEL_DIR, "tokenizer.model"),
+            max_seq_len=params["max_seq_len"],
+            max_batch_size=params["max_batch_size"],
+        )
+        tokenizer = None
+
+    # zero shot
+    prompt_name = "zero_shot_v2"
+    zero_shot_response_path = os.path.join(OUTPUT_DIR, f"{prompt_name}.json")
+    zero_shot_response = {}
+    if os.path.exists(zero_shot_response_path):
+        zero_shot_response = load_json(zero_shot_response_path)
+
+    num_asked = 0
+    for i, (_, row_data) in enumerate(DATA.iterrows()):
+        if num_asked >= NUM_ASK:
+            break
+
+        essay_id = row_data["essay_id"]
+        if essay_id in zero_shot_response.keys():
+            continue
+
+        full_text = row_data["full_text"]
+        prompt = PROMPTS[prompt_name] + "The following is the content of the essay:\n\n" + full_text + "\n"
+
+        if IS_HF:
+            sequences = model(
+                prompt,
+                do_sample=True,
+                top_k=10,
+                num_return_sequences=1,
+                eos_token_id=tokenizer.eos_token_id,
+                truncation=True,
+                max_length=params["max_seq_len"],
+            )
+            generated_response = sequences[0]["generated_text"]
+        else:
+            dialogs = [
+                [{"role": "user", "content": prompt}],
+            ]
+            results = model.chat_completion(
+                dialogs,
+                max_gen_len=None,
+                temperature=params["temperature"],
+                top_p=params["top_p"],
+            )
+            generated_response = results[0]['generation']['content']
+
+        print(f"{get_now_time()}: complete query {i}")
+        has_error, gpt_response, score, explanation = extract_score_explanation(generated_response)
+        zero_shot_response[essay_id] = {
+            "full_text": full_text,
+            "gpt_response": gpt_response,
+            "score_ground_truth": row_data["score"],
+        }
+        if not has_error:
+            zero_shot_response[essay_id]["score_predict"] = score
+            zero_shot_response[essay_id]["chain_of_thought"] = explanation
+        num_asked += 1
+        time.sleep(1)
+    write_json(zero_shot_response, zero_shot_response_path)
---
+++ scripts/llama-2-7b-chat_kaggle_AES2.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+{
+  CUDA_VISIBLE_DEVICES=0 python /home/xiongzj/myProjects/kaggle-AES2-competition/ask_local_llm.py \
+    --prompts_path "/home/xiongzj/myProjects/kaggle-AES2-competition/prompts.json" \
+    --model_dir "/data/xiongzj/LLM/llama2-7b-chat" \
+    --data_dir "/home/xiongzj/myProjects/kaggle-AES2-competition/data" \
+    --output_dir "/home/xiongzj/myProjects/kaggle-AES2-competition/output" \
+    --data_name "kaggle-AES2024" --num_ask 10 \
+    --temperature 0.6 --top_p 0.9 --max_seq_len 4096 --max_batch_size 8
+} >> /home/xiongzj/myProjects/kaggle-AES2-competition/results/llama2-7b-chat_kaggle-AES2024.txt
+
